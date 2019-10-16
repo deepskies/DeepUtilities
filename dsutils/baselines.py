cc79440abb5891eb4cebfd5837eeb145c01d2eb2
anandijain <anandj@uchicago.edu>
2019-10-16 13:21:34 -0500

    baselines takes in path to config now

--- dsutils/baselines.py
+++ dsutils/baselines.py
@@ -1,4 +1,6 @@
 import os.path
+import json
+import time
 
 import h5py
 
@@ -34,9 +36,32 @@ class Baselines:
             * string in ['mlp', 'vae', 'conv1d', # todo - 'conv2d', 'node', 'gan', 'lstm', 'rnn']
 
     '''
-    def __init__(self, dataset_name='mnist', model='mlp', epochs=1, lr=1e-3, classify=True, log_interval=500, factor=5):
+    def __init__(self, path_to_config='./config/mnist.json'):
+        # dataset='mnist', model='mlp', epochs=1, lr=1e-3, classify=True, log_interval=500, factor=5
         # todo: logging for diagnostics
-        self.epochs = epochs
+        with open(path_to_config) as config_file:
+            config = json.load(config_file)
+
+        timestamp = time.strftime("%Y-%m-%d_%H-%M")
+        directory = "./experiments/img_results_{}_{}".format(timestamp, config["id"])
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Save config file in experiment directory
+        # disabled for now
+        # with open(directory + '/config.json', 'w') as config_file:
+        #     json.dump(config, config_file)
+
+        self.model = None
+        dataset = config['dataset']
+        self.path = ds.data.experiment_path(dataset)
+
+        self.model_configs = config['model_configs']
+        self.training_config = config['training_config']
+        self.lr = self.training_config['lr']
+        self.epochs = self.training_config['epochs']
+        self.log_interval = self.training_config['print_freq']
+        self.classify = self.training_config['classify']
         self.logs = {
                         'actual' : [],
                         'predicted' : [],
@@ -44,41 +69,35 @@ class Baselines:
                         'train_acc' : [],
                         'test_acc' : []
                     }
-        self.path = ds.data.experiment_path(dataset_name)
-        self.model_type = model
-        self.log_interval = log_interval
-
-
 
         # for now returns pytorch train_loader, test_loader
-        self.train_loader, self.test_loader = ds.get_dataset(dataset_name)
+        self.train_loader, self.test_loader = ds.get_dataset(dataset)
         self.in_dim, self.out_dim = ds.data.get_dims_from_loader(self.train_loader)
 
-        if model == 'mlp':
-            self.model = mlp.MLP(self.in_dim, self.out_dim, factor=factor)
-        elif model == 'vae':
-            self.model = vae.VAE(self.in_dim, self.out_dim)
-            print('not working yet, needs custom train')
+        self.device = config['device'] # torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
-        self.device = torch.device('cuda:' if torch.cuda.is_available() else 'cpu')
-
-        if classify:
+        if self.classify:
             self.criterion = nn.CrossEntropyLoss()
 
         self.run()
 
     def run(self):
-        if self.model_type == 'mlp': # or others
-            for epoch_id in range(self.epochs):
-                run_epoch.train(self.model, self.device, self.train_loader, self.optimizer, self.criterion, epoch_id, self.log_interval, self.logs)
-                run_epoch.test(self.model, self.device, self.test_loader, self.criterion, self.logs)
-        elif self.model_type == 'vae':
-            run_epoch.vae_train(self.model, self.device, self.train_loader, self.optimizer, self.criterion, epoch_id, self.log_interval)
+        for i, model_config in enumerate(self.model_configs):
+            model = model_config['type']
+            if model == 'automlp':
+                factor = model_config['factor']
+                self.model = mlp.MLP(self.in_dim, self.out_dim, factor=factor).to(self.device)
+                self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
+
+                for epoch_id in range(self.epochs):
+                    run_epoch.train(self.model, self.device, self.train_loader, self.optimizer, self.criterion, epoch_id, self.log_interval, self.logs)
+                    run_epoch.test(self.model, self.device, self.test_loader, self.criterion, self.logs)
+
 
         self.diags = diag.Diagnostics(self.path, self.logs['actual'], self.logs['predicted'])
 
         self.diags.plot_cm()
+
 
 
 def baselines(dataset):
