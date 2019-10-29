0714d830943e7b28e55db97ebf16c4006192a233
Aleksandra Ciprijanovic <43037641+AleksCipri@users.noreply.github.com>
2019-10-29 14:55:24 -0500

    Update plots_gather_aleks.py

--- dsutils/module_report/plots_gather_aleks.py
+++ dsutils/module_report/plots_gather_aleks.py
@@ -722,3 +722,37 @@ def create_report(params, params_model, dirs, figure_dict, data_analysis):
 ''' https://github.com/deepskies/deeplensing-old/blob/master/notebooks/.ipynb_checkpoints/plot_layers-checkpoint.ipynb''' 
 
 
+''' Not for CNN but still a nice plot '''
+''' https://github.com/deepskies/tlens/blob/master/tsne_mnist.ipynb '''
+def plot_mnist(X, y, X_embedded, name, min_dist=10.0):
+    fig = plt.figure(figsize=(10, 10))
+    ax = plt.axes(frameon=False)
+    plt.title("\\textbf{MNIST dataset} -- Two-dimensional "
+          "embedding of 70,000 handwritten digits with %s" % name)
+    plt.setp(ax, xticks=(), yticks=())
+    plt.subplots_adjust(left=0.0, bottom=0.0, right=1.0, top=0.9,
+                    wspace=0.0, hspace=0.0)
+    plt.scatter(X_embedded[:, 0], X_embedded[:, 1], c=y, marker="x")
+    print y.shape
+    print y[0]
+    print len(y)
+    print y.ndim
+    print type(y)
+    if min_dist is not None:
+        from matplotlib import offsetbox
+        shown_images = np.array([[15., 15.]])
+        indices = np.arange(X_embedded.shape[0])
+        np.random.shuffle(indices)
+        for i in indices[:5000]:
+            dist = np.sum((X_embedded[i] - shown_images) ** 2, 1)
+            if np.min(dist) < min_dist:
+                continue
+            shown_images = np.r_[shown_images, [X_embedded[i]]]
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(X[i].reshape(28, 28),
+                                      cmap=plt.cm.gray_r), X_embedded[i])
+            ax.add_artist(imagebox)
+            
+    plt.show()
+    
+   
